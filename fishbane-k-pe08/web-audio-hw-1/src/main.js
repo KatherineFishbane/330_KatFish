e9ea5afb7e6db8084d07285ab2510b50957a6620
Katherine Fishbane <144246177+KatherineFishbane@users.noreply.github.com>
2025-10-06 13:13:01 -0400

    com

---
+++ fishbane-k-pe08/web-audio-hw-1/src/main.js
@@ -0,0 +1,133 @@
+/*
+  main.js is primarily responsible for hooking up the UI to the rest of the application 
+  and setting up the main event loop
+*/
+
+// We will write the functions in this file in the traditional ES5 way
+// In this instance, we feel the code is more readable if written this way
+// If you want to re-write these as ES6 arrow functions, to be consistent with the other files, go ahead!
+import * as audio from './audio.js';
+import * as utils from './utils.js';
+
+
+// 1 - here we are faking an enumeration
+const DEFAULTS = Object.freeze({
+  sound1: "media/New Adventure Theme.mp3"
+});
+
+function init() {
+
+  console.log("init called");
+  console.log(`Testing utils.getRandomColor() import: ${utils.getRandomColor()}`);
+  audio.setupWebaudio(DEFAULTS.sound1);
+  let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
+  setupUI(canvasElement);
+  canvas.setupCanvas(canvasElement,audio.analyserNode);
+  loop();
+}
+
+function setupUI(canvasElement) {
+  // A - hookup fullscreen button
+  const fsButton = document.querySelector("#fsButton");
+
+  // add .onclick event to button
+  fsButton.onclick = e => {
+    console.log("goFullscreen() called");
+    utils.goFullscreen(canvasElement);
+  };
+  playButton.onclick = e => {
+    console.log(`audio.audioCtx state = ${audio.audioCtx.state}`);
+
+    if (audio.audioCtx.state == 'suspended') {
+      audio.audioCtx.resume();
+    }
+    console.log(`audioCtx state = ${audio.audioCtx.state}`);
+    if (e.target.dataset.playing == "no") {
+      audio.playCurrentSound();
+      e.target.dataset.playing = "yes";
+    } else {
+      audio.pauseCurrentSound();
+      e.target.dataset.playing = "no";
+    }
+  };
+  // C - hookup volume slider and label
+  const volumeSlider = document.querySelector("#volumeSlider");
+  const volumeLabel = document.querySelector("#volumeLabel");
+  //add .oninput event to slider
+  volumeSlider.oninput = e => {
+    //set the gain
+    audio.setVolume(e.target.value);
+    //update the value of the label to match value of slider
+    volumeLabel.innerHTML = Math.round((e.target.value / 2 * 100));
+  };
+  //set value of label to match initial value of slider
+  volumeSlider.dispatchEvent(new Event("input"));
+
+  //D - hookup track <select>
+  let trackSelect = document.querySelector("#trackSelect");
+  //add .onchange event to <select>
+  trackSelect.onchange = e => {
+    audio.loadSoundFile(e.target.value);
+    //pause the current track if it is playing
+    if (playButton.dataset.playing == "yes") {
+      playButton.dispatchEvent(new MouseEvent("click"));
+    }
+  };
+  //E - hookup checkboxes
+  let gradientCB = document.querySelector("#gradientCB");
+  let barsCB = document.querySelector("#barsCB");
+  let circlesCB = document.querySelector("#circlesCB");
+  let noiseCB = document.querySelector("#noiseCB");
+
+  gradientCB.checked = drawParams.showGradient;
+  barsCB.checked = drawParams.showBars;
+  circlesCB.checked = drawParams.showCircles;
+  noiseCB.checked = drawParams.showNoise;
+  gradientCB.onchange = e => {
+    drawParams.showGradient = e.target.checked;
+  }
+  barsCB.onchange = e => {
+    drawParams.showBars = e.target.checked;
+  }
+  circlesCB.onchange = e => {
+    drawParams.showCircles = e.target.checked;
+  }
+  noiseCB.onchange = e => {
+    drawParams.showNoise = e.target.checked;
+
+  }
+
+
+
+} // end setupUI
+
+function loop() {
+  /* NOTE: This is temporary testing code that we will delete in Part II */
+  
+  // 1) create a byte array (values of 0-255) to hold the audio data
+  // normally, we do this once when the program starts up, NOT every frame
+  let audioData = new Uint8Array(audio.analyserNode.fftSize / 2);
+
+  // 2) populate the array of audio data *by reference* (i.e. by its address)
+  audio.analyserNode.getByteFrequencyData(audioData);
+
+  // 3) log out the array and the average loudness (amplitude) of all of the frequency bins
+  console.log(audioData);
+
+  console.log("-----Audio Stats-----");
+  let totalLoudness = audioData.reduce((total, num) => total + num);
+  let averageLoudness = totalLoudness / (audio.analyserNode.fftSize / 2);
+  let minLoudness = Math.min(...audioData); // ooh - the ES6 spread operator is handy!
+  let maxLoudness = Math.max(...audioData); // ditto!
+  // Now look at loudness in a specific bin
+  // 22050 kHz divided by 128 bins = 172.23 kHz per bin
+  // the 12th element in array represents loudness at 2.067 kHz
+  let loudnessAt2K = audioData[11];
+  console.log(`averageLoudness = ${averageLoudness}`);
+  console.log(`minLoudness = ${minLoudness}`);
+  console.log(`maxLoudness = ${maxLoudness}`);
+  console.log(`loudnessAt2K = ${loudnessAt2K}`);
+  console.log("---------------------");
+}
+
+export { init };
---
+++ fishbane-k-pe09/src/script.js
@@ -0,0 +1,131 @@
+'use strict';
+
+
+		let ctx = document.querySelector('canvas').getContext('2d');
+		let rotation = 0;
+		let scale = 1;
+		let grow = true;
+
+    const drawSquare1 = (ctx, x, y, width, height, fillStyle) =>{
+			ctx.save();
+			ctx.fillStyle = fillStyle;
+			ctx.fillRect(x, y, width, height);
+			ctx.restore();
+		}
+		const drawSquare2= (ctx, x, y, width, height, rotation, fillStyle, scale) =>{
+			ctx.save();
+			ctx.fillStyle = fillStyle;
+			ctx.translate(x, y);
+			ctx.rotate(rotation);
+			ctx.scale(scale, scale);
+			ctx.fillRect(0 - width / 2, 0 - height / 2, width, height);
+			ctx.restore();
+		}
+		const drawTriangle=(ctx, x, y, width, height, rotation, fillStyle, scale) =>{
+			ctx.save();
+			ctx.fillStyle = fillStyle;
+			ctx.translate(x, y);
+			ctx.rotate(rotation);
+			ctx.scale(scale, scale);
+			ctx.beginPath();
+			ctx.moveTo(0, -height / 2);
+			ctx.lineTo(width / 2, height / 2);
+			ctx.lineTo(-width / 2, height / 2);
+			ctx.closePath();
+			ctx.fill();
+			ctx.restore();
+
+
+
+		}
+
+		//background
+		const loop = () =>{
+			requestAnimationFrame(loop);
+			rotation += 0.05;
+			ctx.fillStyle = 'yellow';
+			ctx.fillRect(0, 0, 640, 480);
+
+			if (grow) {
+				scale += 0.1;
+				if (scale >= 10) {
+					grow = false;
+				}
+			} else {
+				scale -= 0.1;
+				if (scale <= 1) {
+					grow = true;
+				}
+			}
+
+			//do some transformations?
+			//ctx.translate(100,0);
+			//ctx.rotate(Math.PI/4);
+			//ctx.scale(1.2,1.2);
+
+			ctx.save();
+			//set a bunch of state attributes
+			ctx.shadowOffsetX = 15;
+			ctx.shadowOffsetY = 15;
+			ctx.shadowColor = "rgba(153,50,204,.6)";
+			ctx.shadowBlur = 5;
+
+
+			// draw square with fillRect() convenience method
+			ctx.fillStyle = "green";
+			ctx.rotate(rotation);
+			ctx.fillRect(100, 100, 100, 100);
+			ctx.restore();
+
+			//ctx.restore();
+			// // draw square with rect()
+			// ctx.fillStyle="blue";
+			// ctx.beginPath();
+			// ctx.rect(300,100,100,100);
+			// ctx.closePath();
+			// ctx.fill();
+
+			// // draw squares with our helper function
+			// drawSquare1(ctx,100,0,100,100,"orange");
+			// drawSquare1(ctx,250,0,100,100,"orange");
+			// drawSquare1(ctx,400,0,100,100,"orange");
+
+			// ctx.save();
+
+			// // draw a triangle
+			// ctx.strokeStyle="red";
+			// ctx.fillStyle="red";
+			// ctx.lineWidth="5";
+			// ctx.beginPath();
+			// ctx.moveTo(500,100);
+			// ctx.lineTo(550,200);
+			// ctx.lineTo(450,200);
+			// ctx.closePath();
+			// ctx.stroke();
+			// ctx.restore();
+			ctx.save();
+			//draw a "better" triangle - why is this better? You'll see!
+			ctx.strokeStyle = "red";
+			ctx.fillStyle = "red";
+			ctx.lineWidth = "5";
+			ctx.translate(200, 200);
+			ctx.rotate(rotation);
+
+
+			ctx.beginPath();
+			ctx.moveTo(0, -50);
+			ctx.lineTo(50, 50);
+			ctx.lineTo(-50, 50);
+			ctx.closePath();
+			ctx.stroke();
+			ctx.restore();
+
+			drawSquare2(ctx, 100, 100, 9, 19, rotation, "orange", 1);
+			drawSquare2(ctx, 300, 100, 49, 19, rotation, "blue", 1);
+			drawTriangle(ctx, 600, 100, 49, 49, rotation, "orange", scale);
+
+		}
+		loop();
+
+
+		
